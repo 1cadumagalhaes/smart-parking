6ab0e4d6143a3f1d7d800b8f36a5e7d798b7f5dd
magalhaescarlos <magalhaescarlosedu@gmail.com>
2019-08-11 18:21:07 -0300

    Updated environmental impact and fixed navbar bug

--- js/main.js
+++ js/main.js
@@ -50,6 +50,77 @@ $.getJSON('src/data/comoajudar.json',function(data){
     
 });
 
+$.getJSON('src/data/impactoambiental.json',function(data){
+    var dados = data[lang];
+    var titulo = dados['titulo'],
+    conteudo = dados['conteudo'],
+    saude = conteudo['saude'], gases=conteudo['gases'];
+    var cardSaude='',cardGases='';
+
+    
+    $('#impacto-titulo').html(titulo);
+    cardSaude = cardImpactoSaude(saude);
+    cardGases = cardImpactoGases(gases);
+    if(cardSaude)
+        $('#impacto-saude').html(cardSaude);
+    if(cardGases)
+        $('#impacto-gas').html(cardGases);
+
+    
+});
+
+function cardImpactoSaude(content){
+    var titulo=content['titulo'],conteudo=content['conteudo'],collection=content['collection'];
+    function itemlista(item){
+        return '<li class="collection-item transparent"><span>'+item+'</span></li>'
+    };
+    var header = collection['header'], items=collection['items']
+    var lista = items.map(itemlista).join('');
+    
+    var card = '<div class="card transparent">'
+                +'<div class="card-content" >'
+                    +'<div class="card-title">'
+                        +'<h4 class="green-text"><b>'+titulo+'</b></h4> '
+                    +'</div>'
+                    +'<p>'+conteudo+'</p>'
+                    +'<ul class="collection" id="lista-saude">'
+                        +'<li class="collection-header center green-text"><h4>'+header+'</h4></li>'
+                        +lista
+                    +'</ul>'
+                +'</div>'
+            +'</div>';
+    return card;
+}
+
+function cardImpactoGases(content){
+    var titulo=content['titulo'],conteudo=content['conteudo'],table=content['table'];
+    var head = table['head'], body=table['body'];
+
+    var tableTitulo = head.map(tableHead).join('');
+    var tableBody = body.map(tableBodyRow).join('');
+    var card = '<div class="card transparent">'
+                            +'<div class="card-content">'
+                                +'<div class="card-title">'
+                                    +'<h4 class="green-text"><b>'+titulo+'</b></h4> '
+                                +'</div>'
+                                +'<p>'+conteudo+'</p>'
+                                +'<table class="responsive-table highlight">'
+                                    +'<thead>'
+                                        +'<tr>'
+                                            +tableTitulo
+                                        +'</tr>'
+                                    +'</thead>'
+                                    +'<tbody>'
+                                        +tableBody
+                                    +'</tbody>'
+                                +'</table>'
+                            +'</div>'
+                        +'</div>'
+    return card;
+}
+
+
+
 
 
 
@@ -78,7 +149,7 @@ function challengeCard(content){
                     +'<div class="card blue-grey darken-2">'
                         +'<div class="card-content activator white-text">'
                             +'<div class="icon-block">'
-                                +'<h2 class="center "><i class="material-icons">'+icone+'</i></h2>'
+                                +'<h2 class="center "><i class="material-icons">'+icone+'</i><br><br></h2>'
                                 +'<h5 class="center">'+titulo+'</h5>'
                             +'</div>'
                         +'</div>'
@@ -90,4 +161,16 @@ function challengeCard(content){
                     +'</div>'
                 +'</div>';
                 return card;
-            }
+}
+
+function tableHead(item){
+    return '<th><span>'+item+'</span></th>'
+}
+
+function tableBodyItem(item){
+    return '<td><span>'+item+'</span></td>'
+}
+
+function tableBodyRow(item){
+    return '<tr>'+item.map(tableBodyItem).join('')+'</tr>'
+}
